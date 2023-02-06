3a6dbccf6761759a61f0cec141e791da41bc54cc
curedas <curedas.codjo@veone.net>
2023-02-06 17:12:22 +0000

    feat: updates package configuration
    updates package configuration to make it buidable

--- packages/ngx-veone/src/index.ts
+++ packages/ngx-veone/src/index.ts
@@ -1,7 +1,1 @@
-export * from './lib/veone-time-pipe.module';
-
-// RXJS utils
-export { ofDelay } from './lib/pure-functions/rxjs';
-
-// Math utils
-export { randomInteger } from './lib/pure-functions/math';
+export * from './lib/public-api';
